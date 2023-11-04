8f0f9c3f445a5df12efae0781b94c2cd96f873d6
mayajenk <92805893+mayajenk@users.noreply.github.com>
2023-11-04 15:49:06 -0400

    dashboard basic setup

--- health-app-frontend/src/App.js
+++ health-app-frontend/src/App.js
@@ -7,19 +7,46 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to APP TITLE, click a category to get started!
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          // button link need to be edited
         >
-          Learn React
+          Rehabilitation Exercises
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          // button link need to be edited
+        >
+          Evaluate
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          // button link need to be edited
+        >
+          Stay Connected
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          // button link need to be edited
+        >
+          Useful Equiment & Applications
         </a>
       </header>
     </div>
   );
 }
-// memo :)
 export default App;
