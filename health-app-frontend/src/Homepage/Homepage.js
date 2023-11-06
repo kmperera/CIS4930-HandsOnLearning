2629e29a5fa0f4dfd3f7e575a52cea90fa6322d7
Skylar <stewart.skylard@gmail.com>
2023-11-06 17:17:33 -0500

    made links navigate with a click (for some reason you have to click the back button 10 times though wtf)

--- health-app-frontend/src/Homepage/Homepage.js
+++ health-app-frontend/src/Homepage/Homepage.js
@@ -1,48 +1,49 @@
 import "./Homepage.css"
 import React from 'react'
 
+
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 export default function Homepage() {
+    //we use this for routing using the buttons
+    const navigate = useNavigate();
+
+    //handles the click 
+    function handleClick(input) {
+        navigate(input);
+      }
+
+
  return (
     <header className='App-header'>
     <p>
       Welcome to APP TITLE, click a category to get started!
     </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
+    <Link
+      onClick={handleClick('Excercises')}
       // button link need to be edited
     >
       Rehabilitation Exercises
-    </a>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
+    </Link>
+    <Link
+      onClick={handleClick('Excercises')}
       // button link need to be edited
     >
       Evaluate
-    </a>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
+    </Link>
+    <Link
+      onClick={handleClick('Excercises')}
       // button link need to be edited
     >
       Stay Connected
-    </a>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
+    </Link>
+    <Link
+      onClick={handleClick('Excercises')}
       // button link need to be edited
     >
       Useful Equiment & Applications
-    </a>
+    </Link>
     </header>
  );
 }
