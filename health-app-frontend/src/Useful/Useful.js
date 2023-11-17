403c556dd1acd0b373fd10a5a7a1d9830f8c2ef6
Skylar <stewart.skylard@gmail.com>
2023-11-17 10:26:08 -0500

    halfway working version of button

--- health-app-frontend/src/Useful/Useful.js
+++ health-app-frontend/src/Useful/Useful.js
@@ -3,13 +3,14 @@ import Button from '@mui/joy/Button';
 import Select from '@mui/joy/Select';
 import FormLabel from '@mui/joy/FormLabel';
 import Option from '@mui/joy/Option';
+import { useState } from "react";
 
 // when creating new items put them in alphabetically
 const a1 = {
     name: 'Application A',
     imageUrl: 'http://i.imgur.com/yX0vd0Ss.jpg',
     imageSize: 90,
-    type: 'application',
+    type: 'Application',
     description: 'useful for ...',
     price: 4.25,
     link: 'get the product here',
@@ -19,7 +20,7 @@ const e1 = {
     name: 'Equipment A',
     imageUrl: 'http://i.imgur.com/yX0vd0Ss.jpg',
     imageSize: 90,
-    type: 'equipment',
+    type: 'Equipment',
     description: 'useful for ...',
     price: 15.25,
     link: 'get the product here',
@@ -27,81 +28,101 @@ const e1 = {
 
 const AllItems = [a1, e1];
 let listItems = AllItems.map((d) => <li key={d.name}>
-        <h2>{d.name}</h2>
-        <img
-            className="avatar"
-            src={d.imageUrl}
-            alt={'Photo of ' + d.name}
-            style={{
-                width: d.imageSize,
-                height: d.imageSize
-             }}
-        />
-        <p>{d.description}</p>
-        <p>${d.price} at {d.link}</p>
-    </li>);
 
-// this isn't working properly
-function alterList(t, s, h) {
-    if(t == "-" && s == "-" && h == "-") {
-        listItems = AllItems.map((d) => <li key={d.name}>
-        <h2>{d.name}</h2>
-        <img
-            className="avatar"
-            src={d.imageUrl}
-            alt={'Photo of ' + d.name}
-            style={{
-                width: d.imageSize,
-                height: d.imageSize
-             }}
-        />
-        <p>{d.description}</p>
-        <p>${d.price} at {d.link}</p>
-    </li>);
-    }
-    let items = AllItems;
-    if(t !== "-") {
-        // loop through all items
-        items.forEach((item) => {
-            if(t !== item.type) {
-                items.pop(items);
-            }
-        })
-    }
-    if(s !== "-") {
-        // sort elements of items
-    }
-    if(h !== "-") {
-        // loop thorugh all itmes and see if h apears in description
-    }
-    listItems = items.map((d) => <li key={d.name}>
-        <h2>{d.name}</h2>
-        <img
-            className="avatar"
-            src={d.imageUrl}
-            alt={'Photo of ' + d.name}
-            style={{
-                width: d.imageSize,
-                height: d.imageSize
-             }}
-        />
-        <p>{d.description}</p>
-        <p>${d.price} at {d.link}</p>
-    </li>);
-};
+    <h2>{d.name}</h2>
+    <img
+        className="avatar"
+        src={d.imageUrl}
+        alt={'Photo of ' + d.name}
+        style={{
+            width: d.imageSize,
+            height: d.imageSize
+        }}
+    />
+    <p>{d.description}</p>
+    <p>${d.price} at {d.link}</p>
+</li>);
+
 
 export default function Useful() {
+
+    const [list, setList] = useState(<li></li>);
+    const [typeValue, setType] = useState("-");
+    const [sortValue, setSort] = useState("-");
+    const [helpValue, setHelp] = useState("-");
+
+    const handleTypeChange = (event) => {
+        setType(event.target.innerText);
+        console.log(event.target.innerText)
+    }
+
+
+    // this isn't working properly
+    function alterList(t, s, h) {
+        if (t == "-" && s == "-" && h == "-") {
+            listItems = AllItems.map((d) => <li key={d.name}>
+                <h2>{d.name}</h2>
+                <img
+                    className="avatar"
+                    src={d.imageUrl}
+                    alt={'Photo of ' + d.name}
+                    style={{
+                        width: d.imageSize,
+                        height: d.imageSize
+                    }}
+                />
+                <p>{d.description}</p>
+                <p>${d.price} at {d.link}</p>
+            </li>);
+        }
+        let items = AllItems;
+        if (t !== "-") {
+            // loop through all items
+            console.log('value of T: ' + t)
+            items.forEach((item) => {
+                if (t !== item.type) {
+                    items.pop(items);
+                }
+            })
+        }
+        if (s !== "-") {
+            // sort elements of items
+        }
+        if (h !== "-") {
+            // loop thorugh all itmes and see if h apears in description
+        }
+        listItems = items.map((d) => <li key={d.name}>
+            <h2>{d.name}</h2>
+            <img
+                className="avatar"
+                src={d.imageUrl}
+                alt={'Photo of ' + d.name}
+                style={{
+                    width: d.imageSize,
+                    height: d.imageSize
+                }}
+            />
+            <p>{d.description}</p>
+            <p>${d.price} at {d.link}</p>
+        </li>);
+        setList(listItems);
+        console.log(listItems);
+    };
+
+
     // this isn't working properly
     function search() {
-        {alterList("application", "-", "-")}
+        { alterList(typeValue, sortValue, helpValue) }
     }
+
     
+
     return (
         <div>
             <h1>Useful Equipment & Applications (Under Construction)</h1>
             <FormLabel>
                 <FormLabel>Type:
-                    <Select name="type">
+                    <Select name="type" onChange = {handleTypeChange}>
                         <Option value="empty">-</Option>
                         <Option value="equipment">Equipment</Option>
                         <Option value="applications">Applications</Option>
@@ -124,6 +145,7 @@ export default function Useful() {
                 </FormLabel>
                 <Button onClick={() => search()}>Search</Button>
             </FormLabel>
+            {list}
         </div>
     );
 }
