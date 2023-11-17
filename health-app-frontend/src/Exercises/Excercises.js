d0fdbc51a1232691550d32e7dc34cf7193467077
Katie <katieperera01@gmail.com>
2023-11-16 19:22:32 -0500

    testing

--- health-app-frontend/src/Exercises/Excercises.js
+++ health-app-frontend/src/Exercises/Excercises.js
@@ -1,9 +1,33 @@
-import React from "react"
+import React from "react";
 
 export default function Exercises() {
     return (
         <div>
-            <h1>This is the excercises page (Under Construction)</h1>
+            <h1>This is the exercises page (Under Construction)</h1>
+
+            {/* Table */}
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Category</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Exercise 1</td>
+                        <td>Category A</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Exercise 2</td>
+                        <td>Category B</td>
+                    </tr>
+                    {/* Add more rows as needed */}
+                </tbody>
+            </table>
         </div>
     );
 }
